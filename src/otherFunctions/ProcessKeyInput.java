ba44711cbcd40af33655f39542af0a3ce0d849a5
AimilianosKass <aimilianoskas@gmail.com>
2021-02-17 21:27:54 +0200

    Yaw and throttle functions added
    +In processKeyInput class yaw and throttle functions added
    
    +Names has been changed for better code reading

--- src/otherFunctions/ProcessKeyInput.java
+++ src/otherFunctions/ProcessKeyInput.java
@@ -8,60 +8,81 @@ public class ProcessKeyInput
 {
 	private static final int MOTOR_STEP = 10;
 	
-	private int L_R = 11500;
-	private int F_B = 21500;
-	
-	private Set<Integer> returnValues ;
-	
+	private int roll = 11500;
+	private int pitch = 21500;
+	private int yaw = 31500;
+	private int throttle = 41500;
 	
 	
 	public Set<Integer> values(Set<Integer> keys)
 	{
+		Set<Integer> returnValues = new HashSet<>();
 		
-		returnValues = new HashSet<>();
 		
 		if (!keys.contains(KeyEvent.VK_SHIFT))
 		{
-			// TODO: 2/5/2021  yaw , throttle , roll , pitch
-			if (keys.contains(KeyEvent.VK_W)) //FORWARD
+			if (keys.contains(KeyEvent.VK_W)) //PITCH FORWARD
 			{
-				if (F_B <22000)
-					F_B += MOTOR_STEP;
+				if (pitch < 22000)
+					pitch += MOTOR_STEP;
 			}
-			if (keys.contains(KeyEvent.VK_S)) //BACKWARD
+			if (keys.contains(KeyEvent.VK_S)) //PITCH BACKWARD
 			{
-				if (F_B >21000)
-					F_B -= MOTOR_STEP;
+				if (pitch > 21000)
+					pitch -= MOTOR_STEP;
 			}
-			if (keys.contains(KeyEvent.VK_A)) //LEFT
+			if (keys.contains(KeyEvent.VK_A)) //ROLL LEFT
 			{
-				if (L_R >11000)
-					L_R -= MOTOR_STEP;
+				if (roll > 11000)
+					roll -= MOTOR_STEP;
 			}
-			if (keys.contains(KeyEvent.VK_D)) //RIGHT
+			if (keys.contains(KeyEvent.VK_D)) //ROLL RIGHT
 			{
-				if (L_R <12000)
-					L_R += MOTOR_STEP;
+				if (roll < 12000)
+					roll += MOTOR_STEP;
+			}
+			if (keys.contains(KeyEvent.VK_Q)) //YAW LEFT
+			{
+				if (yaw > 41000)
+					yaw -= MOTOR_STEP;
+			}
+			if (keys.contains(KeyEvent.VK_E)) //YAW RIGHT
+			{
+				if (yaw < 42000)
+					yaw += MOTOR_STEP;
+			}
+			if (keys.contains(KeyEvent.VK_UP)) //THROTTLE UP
+			{
+				if (throttle < 32000)
+					throttle += MOTOR_STEP;
+			}
+			if (keys.contains(KeyEvent.VK_DOWN)) //THROTTLE DOWN
+			{
+				if (throttle > 31000)
+					throttle -= MOTOR_STEP;
 			}
 		}
 		
+		boolean stopPitch = !keys.contains(KeyEvent.VK_W) && !keys.contains(KeyEvent.VK_S);
+		boolean stopRoll = !keys.contains(KeyEvent.VK_A) && !keys.contains(KeyEvent.VK_D);
+		boolean stopYaw = !keys.contains(KeyEvent.VK_Q) && !keys.contains(KeyEvent.VK_E);
+		boolean stopThrottle = !keys.contains(KeyEvent.VK_UP) && !keys.contains(KeyEvent.VK_DOWN);
 		
-		boolean stopLR = !keys.contains(KeyEvent.VK_A) && !keys.contains(KeyEvent.VK_D);
 		
-		boolean stopFB = !keys.contains(KeyEvent.VK_W) && !keys.contains(KeyEvent.VK_S);
+		if (stopRoll)
+			roll = 11500;
+		if (stopPitch)
+			pitch = 21500;
+		if (stopYaw)
+			yaw = 31500;
+		if (stopThrottle)
+			throttle = 41500;
 		
 		
-		if (stopLR)
-		{
-			L_R = 11500;
-		}
-		if (stopFB)
-		{
-			F_B = 21500;
-		}
-		
-		returnValues.add(L_R);
-		returnValues.add(F_B);
+		returnValues.add(roll);
+		returnValues.add(pitch);
+		returnValues.add(yaw);
+		returnValues.add(throttle);
 		
 		
 		return returnValues;
